abf3503c660edfb93e127006b6842584a7f7a0c2
Anmol Patel <anmolpatel86791@gmail.com>
2026-04-26 22:06:42 +0530

    Code Challenge 2

---
+++ code-challenge2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int studentID = 12305086;
+    double score = 98.9;
+    char grade = 'A';
+    cout<<"Student ID: "<<studentID<<endl;
+    cout<<"Score: "<<score<<endl;
+    cout<<"Grade: "<<grade<<endl;
+    return 0;
+}
