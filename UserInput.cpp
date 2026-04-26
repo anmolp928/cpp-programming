256d290117ec8534f46d9f869ad25caf04b58705
Anmol Patel <anmolpatel86791@gmail.com>
2026-04-26 21:11:34 +0530

    User Input

---
+++ UserInput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter x: "<<endl;
+    cin>>x;
+    cout<<"Enter y: "<<endl;
+    cin>>y;
+    int sum = x+y;
+    cout<<"Value of Sum is: "<<sum;
+    return 0;
+}
