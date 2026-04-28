29263108c8c8bb2795867172b624f1e7695026b8
Anmol Patel <anmolpatel86791@gmail.com>
2026-04-28 14:40:48 +0530

    Code Challenge 3

---
+++ code-challenge3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int itemPrice = 50;
+    int shippingCost = 20;
+    int sum = itemPrice + shippingCost;
+    cout<<sum<<endl;
+    return 0;
+}
